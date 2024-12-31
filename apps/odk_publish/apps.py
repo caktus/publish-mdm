6a427f5653efbb5b1495febb0a9cfdc4e2b39f5e
Colin Copeland <copelco@caktusgroup.com>
2024-12-31 09:38:33 -0500

    set verbose name to ODK Publish

--- apps/odk_publish/apps.py
+++ apps/odk_publish/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class OdkPublishConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.odk_publish"
+    verbose_name = "ODK Publish"
