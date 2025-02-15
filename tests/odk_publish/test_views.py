0a31c12a8f97b80877c1eb5766ffb2f9d3d96ccc
Simon Kagwi <skagwi@caktusgroup.com>
2025-02-15 03:17:37 +0300

    Add some tests for the import view.

--- tests/odk_publish/test_views.py
+++ tests/odk_publish/test_views.py
@@ -1,5 +1,12 @@
+import os
+import shutil
+from io import StringIO
+
 import pytest
 from django.urls import reverse
+from django.conf import settings
+from import_export.formats.base_formats import CSV
+from import_export.tmp_storages import MediaStorage
 
 from tests.odk_publish.factories import (
     AppUserFormTemplateFactory,
@@ -7,12 +14,12 @@ from tests.odk_publish.factories import (
     ProjectFactory,
     UserFactory,
 )
+from apps.odk_publish.forms import AppUserConfirmImportForm
+from apps.odk_publish.models import AppUser
 
 
 @pytest.mark.django_db
-class TestPublishTemplate:
-    """Test the PublishTemplateForm form validation."""
-
+class ViewTestBase:
     @pytest.fixture
     def user(self, client):
         user = UserFactory()
@@ -24,6 +31,18 @@ class TestPublishTemplate:
     def project(self):
         return ProjectFactory()
 
+    def test_login_required(self, client, url):
+        response = client.get(url)
+        assert response.status_code == 302
+
+    def test_get(self, client, url, user):
+        response = client.get(url)
+        assert response.status_code == 200
+
+
+class TestPublishTemplate(ViewTestBase):
+    """Test the PublishTemplateForm form validation."""
+
     @pytest.fixture
     def form_template(self, project):
         return FormTemplateFactory(project=project)
@@ -34,14 +53,6 @@ class TestPublishTemplate:
             "odk_publish:form-template-publish",
             kwargs={"odk_project_pk": project.pk, "form_template_id": form_template.pk},
         )
-
-    def test_login_required(self, client, url):
-        response = client.get(url)
-        assert response.status_code == 302
-
-    def test_get(self, client, url, user):
-        response = client.get(url)
-        assert response.status_code == 200
 
     def test_post(self, client, url, user, project, form_template):
         app_user = AppUserFormTemplateFactory(
@@ -61,3 +72,73 @@ class TestPublishTemplate:
         assert response.status_code == 200
         # Check that the response triggers the WebSocket connection
         assert 'hx-ws="send"' in str(response.content)
+
+
+class TestImport(ViewTestBase):
+    """Test the AppUser import process."""
+
+    @pytest.fixture
+    def url(self, project):
+        return reverse(
+            "odk_publish:app-users-import",
+            kwargs={"odk_project_pk": project.pk},
+        )
+
+    @pytest.fixture
+    def csv_data(self):
+        """Valid CSV import data with one new user."""
+        return "id,name,central_id,form_templates\n" ",newuser,,\n"
+
+    def teardown_class(cls):
+        shutil.rmtree(os.path.join(settings.MEDIA_ROOT, "django-import-export"))
+
+    def test_valid_upload(self, client, url, user, csv_data):
+        """Ensure the review page is shown after a valid import file is uploaded."""
+        data = {"format": 0, "import_file": StringIO(csv_data)}
+        response = client.post(url, data=data)
+        assert response.status_code == 200
+        assert isinstance(response.context["form"], AppUserConfirmImportForm)
+        assert (
+            "Below is a preview of data to be imported. If you are satisfied "
+            "with the results, click 'Confirm import'."
+        ) in response.content.decode()
+        assert len(response.context["result"].valid_rows()) == 1
+        # The user should not be created yet
+        assert AppUser.objects.count() == 0
+        # The CSV data should be saved in a temp file
+        import_format = CSV("utf-8-sig")
+        tmp_storage = MediaStorage(
+            name=response.context["form"].initial["import_file_name"],
+            encoding=import_format.encoding,
+            read_mode=import_format.get_read_mode(),
+        )
+        assert tmp_storage.read().decode(import_format.encoding) == csv_data
+
+    def test_import_confirmed(self, client, url, user, csv_data, project):
+        """Ensure confirming the import in the review page updates users."""
+        # Save the CSV data in a temp file, as would happen when a valid file is uploaded
+        import_format = CSV("utf-8-sig")
+        tmp_storage = MediaStorage(
+            encoding=import_format.encoding,
+            read_mode=import_format.get_read_mode(),
+        )
+        tmp_storage.save(csv_data)
+        # Submit a form for confirming the import
+        data = {
+            "import_file_name": tmp_storage.name,
+            "original_file_name": "test.csv",
+            "format": 0,
+            "resource": "",
+        }
+        response = client.post(url, data=data, follow=True)
+        # A new user is created and there's a redirect to the user list page
+        # with a success message
+        assert response.status_code == 200
+        assert AppUser.objects.count() == 1
+        assert response.redirect_chain == [
+            (reverse("odk_publish:app-user-list", args=[project.id]), 302)
+        ]
+        assert (
+            "Import finished successfully, with 1 new and 0 updated app users."
+            in response.content.decode()
+        )
