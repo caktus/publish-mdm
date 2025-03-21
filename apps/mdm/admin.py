b400ef229ecd856512ff46ea5600cc308f069d76
Simon Kagwi <skagwi@caktusgroup.com>
2025-03-22 01:15:47 +0300

    Add import/export functionality for the mdm.Device model.

--- apps/mdm/admin.py
+++ apps/mdm/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from import_export.admin import ImportExportMixin
+from import_export.forms import ExportForm
+from import_export.resources import ModelResource
+
 from .models import Policy, Device
 
 
@@ -9,9 +13,21 @@ class PolicyAdmin(admin.ModelAdmin):
     list_filter = ("project",)
 
 
+class DeviceResource(ModelResource):
+    """Custom ModelResource for importing/exporting Devices."""
+
+    class Meta:
+        model = Device
+        fields = ("id", "policy", "serial_number", "app_user_name", "device_id")
+        clean_model_instances = True
+        skip_unchanged = True
+
+
 @admin.register(Device)
-class DeviceAdmin(admin.ModelAdmin):
+class DeviceAdmin(ImportExportMixin, admin.ModelAdmin):
     list_display = ("name", "serial_number", "app_user_name", "policy")
     search_fields = ("serial_number", "app_user_name", "policy__name", "serial_number")
     readonly_fields = ("name", "device_id", "raw_mdm_device")
     list_filter = ("policy", "app_user_name")
+    export_form_class = ExportForm
+    resource_classes = [DeviceResource]
