64b3c0b4cf51a4dfcd9453c8fd0fdf3c38e01619
Simon Kagwi <skagwi@caktusgroup.com>
2025-04-17 22:43:00 +0300

    Fix unit tests after merging main into this branch.

--- tests/mdm/test_tasks.py
+++ tests/mdm/test_tasks.py
@@ -1,5 +1,6 @@
 import json
 import pytest
+import faker
 from requests.sessions import Session
 
 from apps.mdm import tasks
@@ -7,6 +8,8 @@ from apps.odk_publish.etl.odk.constants import DEFAULT_COLLECT_SETTINGS
 from tests.odk_publish.factories import AppUserFactory
 
 from .factories import DeviceFactory, PolicyFactory
+
+fake = faker.Faker()
 
 
 @pytest.mark.django_db
@@ -36,6 +39,24 @@ class TestTasks:
         for var in self.TINYMDM_ENV_VARS:
             monkeypatch.setenv(var, "test")
 
+    def get_fake_device_data(self):
+        data = {
+            "last_sync_timestamp": int(fake.past_datetime().timestamp()),
+            "manufacturer": fake.company(),
+            "enrollment_type": fake.random_element(["fully_managed", "work_profile"]),
+            "os_version": fake.android_platform_token(),
+            "battery_level": fake.pyint(0, 100),
+        }
+        if fake.pybool():
+            data["geolocation_positions"] = [
+                {
+                    "latitude": float(fake.latitude()),
+                    "longitude": float(fake.longitude()),
+                }
+                for _ in range(2)
+            ]
+        return data
+
     def get_raw_mdm_device(self, device):
         """Create data for the Device.raw_mdm_device field based on other fields' values."""
         return {
@@ -44,6 +65,7 @@ class TestTasks:
             "name": device.name,
             "nickname": device.name,
             "user_id": f"user{device.device_id}",
+            **self.get_fake_device_data(),
         }
 
     @pytest.fixture
@@ -62,6 +84,7 @@ class TestTasks:
                     "name": f"updated-name-{device.name}",
                     "nickname": f"updated-nickname-{device.name}",
                     "user_id": f"user{device.device_id}",
+                    **self.get_fake_device_data(),
                 }
                 for device in devices
             ]
@@ -109,6 +132,24 @@ class TestTasks:
     def test_pull_devices(self, policy, requests_mock, devices_response, set_tinymdm_env_vars):
         """Ensures calling pull_devices() updates and creates Devices as expected."""
         requests_mock.get("https://www.tinymdm.net/api/v1/devices", json=devices_response)
+        apps = {}
+        for device in devices_response["results"]:
+            apps[device["id"]] = [
+                {
+                    "package_name": f"org.{fake.word()}.{fake.word()}",
+                    "app_name": fake.sentence(),
+                    "version_code": fake.pyint(),
+                    "version_name": fake.word(),
+                }
+                for _ in range(3)
+            ]
+            requests_mock.get(
+                f"https://www.tinymdm.net/api/v1/devices/{device['id']}/apps",
+                json={
+                    "results": apps[device["id"]],
+                },
+            )
+
         session = tasks.get_tinymdm_session()
         tasks.pull_devices(session, policy)
 
@@ -121,6 +162,30 @@ class TestTasks:
             assert db_device.serial_number == device["serial_number"]
             assert db_device.name == device["nickname"] or device["name"]
             assert db_device.raw_mdm_device == device
+
+            # Ensure a snapshot has been saved with the expected data
+            snapshot = db_device.latest_snapshot
+            assert snapshot is not None
+            assert snapshot.mdm_device == db_device
+            assert snapshot.raw_mdm_device == device
+            assert snapshot.serial_number == device["serial_number"]
+            assert snapshot.name == device["nickname"] or device["name"]
+            assert snapshot.last_sync.timestamp() == device["last_sync_timestamp"]
+
+            for key in ("manufacturer", "os_version", "battery_level", "enrollment_type"):
+                assert getattr(snapshot, key) == device[key]
+
+            geolocation_positions = device.get("geolocation_positions")
+            if geolocation_positions:
+                assert snapshot.latitude == geolocation_positions[-1]["latitude"]
+                assert snapshot.longitude == geolocation_positions[-1]["longitude"]
+
+            # Ensure the apps have been saved with the expected data
+            assert set(tuple(i.values()) for i in apps[device["id"]]) == set(
+                snapshot.apps.values_list(
+                    "package_name", "app_name", "version_code", "version_name"
+                )
+            )
 
     @pytest.mark.parametrize("device", [False, True], indirect=True)
     def test_push_device_config(self, device, requests_mock, set_tinymdm_env_vars):
@@ -175,9 +240,24 @@ class TestTasks:
         # push_device_config should be called only for the devices where
         # app_user_name is set
         mock_push_device_config.assert_has_calls(
-            [mocker.call(session, device) for device in devices_to_push], any_order=True
+            [mocker.call(session=session, device=device) for device in devices_to_push],
+            any_order=True,
         )
         assert mock_push_device_config.call_count == len(devices_to_push)
+
+    def test_sync_policy_with_push_config_false(
+        self, policy, devices, mocker, set_tinymdm_env_vars
+    ):
+        """Ensure calling sync_policy() with push_config=False calls pull_devices()
+        for the specified policy but does not call push_device_config() for any device.
+        """
+        mock_pull_devices = mocker.patch("apps.mdm.tasks.pull_devices")
+        mock_push_device_config = mocker.patch("apps.mdm.tasks.push_device_config")
+        session = tasks.get_tinymdm_session()
+        tasks.sync_policy(session, policy, push_config=False)
+
+        mock_pull_devices.assert_called_once()
+        mock_push_device_config.assert_not_called()
 
     def test_sync_policies(self, policies, mocker, set_tinymdm_env_vars):
         """Ensure calling sync_policies() calls sync_policy() for all policies."""
