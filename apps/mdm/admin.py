98ed8c9d435f685a898fa186c38440fbbe64b15c
Simon Kagwi <skagwi@caktusgroup.com>
2025-04-17 19:30:19 +0300

    Fix handling of errors during the confirm step of importing mdm.Device data.

--- apps/mdm/admin.py
+++ apps/mdm/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.contrib import messages
+from django.db import transaction
+from django.utils.html import mark_safe
 from import_export.admin import ImportExportMixin
 from import_export.forms import ExportForm
 
@@ -21,3 +24,27 @@ class DeviceAdmin(ImportExportMixin, admin.ModelAdmin):
     list_filter = ("policy", "app_user_name")
     export_form_class = ExportForm
     resource_classes = [DeviceResource]
+
+    def process_result(self, result, request):
+        if request.path.endswith("/process_import/"):
+            # If some errors occur during the confirm import step, show error messages
+            for row in result.error_rows:
+                for error in row.errors:
+                    messages.error(
+                        request,
+                        mark_safe(
+                            f"Row {row.number}: {error.error!r}<br><pre>{error.traceback}</pre>"
+                        ),
+                    )
+            for row in result.invalid_rows:
+                for field, errors in row.error_dict.items():
+                    for error in errors:
+                        messages.error(request, f"Row {row.number}, Column '{field}': {error}")
+            if result.has_errors():
+                # Save successful rows, since all DB changes will have been rolled back
+                # by DeviceResource.import_data()
+                with transaction.atomic():
+                    for row in result.valid_rows():
+                        # We already successfully pushed to MDM
+                        row.instance.save(push_to_mdm=False)
+        return super().process_result(result, request)
