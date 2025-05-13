7f4bb6aeed44f2cf9aa599914cda5657c0476e7f
Simon Kagwi <skagwi@caktusgroup.com>
2025-05-14 00:26:17 +0300

    Fix custom database fields errors when saving in Admin.

--- apps/patterns/fields.py
+++ apps/patterns/fields.py
@@ -6,30 +6,9 @@ from .infisical import kms_api
 class EncryptedMixin(object):
     # Based on https://gitlab.com/lansharkconsulting/django/django-encrypted-model-fields/-/blob/develop/encrypted_model_fields/fields.py#L64
 
-    def to_python(self, value):
-        if value is None:
-            return value
-
-        if isinstance(value, (bytes, str)):
-            if isinstance(value, bytes):
-                value = value.decode("utf-8")
-
-            value = kms_api.decrypt(self.model.__name__.lower(), value)
-
-        return super(EncryptedMixin, self).to_python(value)
-
-    def get_prep_value(self, value):
-        """This method gets called when saving a value in the database, and it in
-        turn calls to_python(). We need to avoid calling EncryptedMixin.to_python()
-        here as it would try to decrypt a value that has not yet been encrypted,
-        causing Infisical to raise an APIError.
-        """
-        # Recreate CharField.get_prep_value()
-        # https://github.com/django/django/blob/3176904/django/db/models/fields/__init__.py#L1297
-        value = models.Field.get_prep_value(self, value)
-        return models.CharField.to_python(self, value)
-
     def from_db_value(self, value, *args, **kwargs):
+        if value:
+            return kms_api.decrypt(self.model.__name__.lower(), value)
         return self.to_python(value)
 
     def get_db_prep_save(self, value, connection):
