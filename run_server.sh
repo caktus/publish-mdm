7b64b3bf9987e01e73032a082b64c53f2a20d8d8
Simon Kagwi <skagwi@caktusgroup.com>
2025-06-11 20:27:46 +0300

    Remove USE_INFISICAL_RUN and rename INFISICAL_SECRETS_FILE.

--- run_server.sh
+++ run_server.sh
@@ -1,26 +1,14 @@
 #!/bin/sh
 
-if [ "$USE_GUNICORN" ]; then
-    alias run_server="newrelic-admin run-program gunicorn config.wsgi --bind 0.0.0.0 --config python:config.gunicorn"
-else
-    alias run_server="newrelic-admin run-program daphne config.asgi:application --bind 0.0.0.0 --port 8000"
-fi
-
-if [ "$INFISICAL_SECRETS_FILE" ]; then
-    echo "Adding Infisical secrets to the environment..."
-    export $(cat $INFISICAL_SECRETS_FILE | xargs)
+if [ "$ENV_VARS_FILE" ]; then
+    echo "Adding environment variables from $ENV_VARS_FILE ..."
+    export $(cat $ENV_VARS_FILE | xargs)
 fi
 
 set -x
 
-if [ "$USE_INFISICAL_RUN" ]; then
-    # Run the application with `infisical run`, which will inject secrets into
-    # the application process as environment variables.
-    # See:
-    # - https://infisical.com/docs/cli/usage#staging-production-and-all-other-use-cases
-    # - https://infisical.com/docs/integrations/platforms/docker
-    # - https://infisical.com/docs/cli/commands/run
-    infisical run --projectId $INFISICAL_SECRETS_PROJECT_ID --env $INFISICAL_SECRETS_ENV --silent -- run_server
+if [ "$USE_GUNICORN" ]; then
+    newrelic-admin run-program gunicorn config.wsgi --bind 0.0.0.0 --config python:config.gunicorn
 else
-    run_server
+    newrelic-admin run-program daphne config.asgi:application --bind 0.0.0.0 --port 8000
 fi
