6251006b1c230f318930ad20edfdcc9b3d353b53
Simon Kagwi <skagwi@caktusgroup.com>
2025-06-27 18:44:28 +0300

    Add pagination, searching, and filtering by Fleet on the Device list page.

--- apps/publish_mdm/views.py
+++ apps/publish_mdm/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.postgres.aggregates import ArrayAgg
 from django.db import models, transaction
 from django.db.models import OuterRef, Q, Subquery, Value
-from django.db.models.functions import Lower, NullIf
+from django.db.models.functions import Collate, Lower, NullIf
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils.html import mark_safe
@@ -47,6 +47,7 @@ from .etl.load import (
     sync_central_project,
 )
 
+from .filters import DeviceFilter
 from .forms import (
     ProjectSyncForm,
     AppUserConfirmImportForm,
@@ -65,6 +66,7 @@ from .forms import (
     CentralServerFrontendForm,
     DeviceEnrollmentQRCodeForm,
     BYODDeviceEnrollmentForm,
+    SearchForm,
 )
 from .import_export import AppUserResource
 from .models import (
@@ -857,8 +859,21 @@ def devices_list(request: HttpRequest, organization_slug):
         )
         .select_related("latest_snapshot")
     )
-    table = DeviceTable(data=devices, show_footer=False)
-    RequestConfig(request, paginate=False).configure(table)
+    search_form = SearchForm(request.GET)
+
+    if search_form.is_valid() and (search_term := search_form.cleaned_data["search"]):
+        # Can't do a "contains" query on app_user_name as it has a nondeterministic collation.
+        # Create an annotation with a deterministic collation that we can do the query on.
+        devices = devices.annotate(
+            app_user_name_deterministic=Collate("app_user_name", "und-x-icu")
+        )
+        q = Q()
+        for field in ["name", "serial_number", "device_id", "app_user_name_deterministic"]:
+            q |= Q(**{f"{field}__icontains": search_term})
+        devices = devices.filter(q)
+
+    filter_ = DeviceFilter(request.GET, queryset=devices)
+    table = DeviceTable(data=filter_.qs, request=request, show_footer=False)
     context = {
         "table": table,
         "breadcrumbs": Breadcrumbs.from_items(
@@ -868,6 +883,8 @@ def devices_list(request: HttpRequest, organization_slug):
         "enroll_form": DeviceEnrollmentQRCodeForm(request.organization),
         "byod_form": BYODDeviceEnrollmentForm(request.organization, prefix="byod"),
         "devices_list_messages": devices_list_messages,
+        "filter": filter_,
+        "search_form": search_form,
     }
 
     template = "publish_mdm/devices_list.html"
