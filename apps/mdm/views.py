50307970e33ec2ae000fdc38a59193749e0b0d69
Tobias McNulty <tobias@caktusgroup.com>
2026-03-23 12:37:26 -0400

    collapse migrations, fix pre-commit

--- apps/mdm/views.py
+++ apps/mdm/views.py
@@ -39,7 +39,7 @@ logger = structlog.get_logger()
 def firmware_snapshot_view(request):
     api_key = settings.MDM_FIRMWARE_API_KEY
     if api_key:
-        auth_header = request.META.get("HTTP_AUTHORIZATION", "")
+        auth_header = request.headers.get("authorization", "")
         if auth_header != f"Bearer {api_key}":
             return HttpResponse(status=401)
     else:
